55366f9f6572bacb9d7829347fc6b2a1d03e96dc
Abdelhaque Id Ali <abdelhaqueidali@users.noreply.huggingface.co>
2025-02-19 04:03:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,6 @@ def search_dglai14(start_search_term_general, contain_search_term_general,start_
         LEFT JOIN expression ON lexie.id_lexie = expression.id_lexie
         WHERE
         (NORMALIZE_AMAZIGH(lexie) LIKE ?) -- Use NORMALIZE_AMAZIGH for lexie (Amazigh word)
-        OR (REMOVE_DIACRITICS(LOWER(api)) LIKE ?)
         OR (REMOVE_DIACRITICS(LOWER(remarque)) LIKE ?)
         OR (REMOVE_DIACRITICS(LOWER(variante)) LIKE ?)
         OR (REMOVE_DIACRITICS(LOWER(cg)) LIKE ?)
@@ -135,7 +134,6 @@ def search_dglai14(start_search_term_general, contain_search_term_general,start_
         LEFT JOIN expression ON lexie.id_lexie = expression.id_lexie
         WHERE (
         (NORMALIZE_AMAZIGH(lexie) LIKE ?) -- Use NORMALIZE_AMAZIGH for lexie (Amazigh word)
-        OR (REMOVE_DIACRITICS(LOWER(api)) LIKE ?)
         OR (REMOVE_DIACRITICS(LOWER(remarque)) LIKE ?)
         OR (REMOVE_DIACRITICS(LOWER(variante)) LIKE ?)
         OR (REMOVE_DIACRITICS(LOWER(cg)) LIKE ?)
@@ -223,7 +221,6 @@ def format_dglai14_results(results):
         if lexie_id not in aggregated_results:
             aggregated_results[lexie_id] = {
                 'lexie': row['lexie'],
-                'api': row['api'],
                 'remarque': row['remarque'],
                 'variante': row['variante'],
                 'cg': row['cg'],
@@ -265,7 +262,6 @@ def format_dglai14_results(results):
         """
 
         fields = {
-            'Transcription': 'api',
             'Notes': 'remarque',
             'Construct State': 'eadata',
             'Plural': 'pldata',
